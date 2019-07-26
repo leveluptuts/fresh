798dd6a7cf0a58216c3ae9ad3334d90597b88dcb
Scott Tolinski <scott.tolinski@gmail.com>
2019-07-26 17:37:50 -0600

    minor refactoring and readme updates

--- example/src/App.js
+++ example/src/App.js
@@ -3,19 +3,24 @@ import { Form, Field } from 'fresh-forms';
 
 const options = ['prerelease', 'active', 'retired', 'hidden'];
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>YO That's Fresh</h1>
-        <Form onSubmit={() => {}}>
-          <Field>Name</Field>
-          <Field type="number">Number</Field>
-          <Field type="select" options={options}>
-            Type
-          </Field>
-        </Form>
-      </div>
-    );
-  }
-}
+const App = () => {
+  // The on submit function is passed a data object with form data
+  const onSubmit = ({ data }) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>YO That's Fresh</h1>
+      <Form onSubmit={onSubmit}>
+        <Field>Name</Field>
+        <Field type="number">Number</Field>
+        <Field type="select" options={options}>
+          Type
+        </Field>
+      </Form>
+    </div>
+  );
+};
+
+export default App;
