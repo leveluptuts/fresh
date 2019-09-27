05718fffb3fe2db0634b427528bfd97fac59d23b
EricSSartorius <EricSSartorius@gmail.com>
2019-09-27 13:45:30 -0700

    Add tooltip and fix tag style

--- src/style.tsx
+++ src/style.tsx
@@ -1,118 +1,151 @@
 import { createGlobalStyle } from 'styled-components'
 
 const Global = createGlobalStyle`
-.fresh-markdown-wrapper {
+  .fresh-markdown-wrapper {
+    .fresh-input-textarea,
+    .fresh-input-markdown {
+      width: 100%;
+    }
+    @media (min-width: 800px) {
+      display: flex;
+      .fresh-input-textarea {
+        width: 50%;  
+      }
+      
+      .fresh-input-markdown {
+        width: calc(50% - 0.5em);  
+        margin-left: 0.5em;
+      }
+    }
+  }
+
+  .fresh-switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+    margin: 0.5em 0px;
+  }
+
+  .fresh-switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  .fresh-slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    transition: 0.4s;
+    border-radius: 34px;
+  }
+
+  .fresh-slider:before {
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    transition: 0.4s;
+    border-radius: 50%;
+  }
+
+  input:checked + .fresh-slider {
+    background-color: #2196f3;
+  }
+
+  input:focus + .fresh-slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + .fresh-slider:before {
+    transform: translateX(26px);
+  }
+
+  input,
+  select,
+  textarea,
+  .fresh-input {
+    background-color: transparent;
+    box-sizing: border-box;
+    box-shadow: none;
+    max-width: 100%;
+    border-width: 1px;
+    border-style: solid;
+    border-image: initial;
+    border-color: inherit;
+    border-radius: 3px;
+    font: inherit;
+    margin: 0.5em 0px;
+    padding: 0.75em;
+    transition: border-color 0.2s ease 0s;
+  }
+
+  textarea,
   .fresh-input-textarea,
   .fresh-input-markdown {
-    width: 100%;
+    min-height: 6rem;
   }
-  @media (min-width: 800px) {
-    display: flex;
-    .fresh-input-textarea {
-      width: 50%;  
-    }
-    
-    .fresh-input-markdown {
-      width: calc(50% - 0.5em);  
-      margin-left: 0.5em;
+
+  .fresh-input-markdown {
+    border-color: #eee; 
+  }
+
+  .fresh-title {
+    display: flex; 
+    align-items: center;
+    .fresh-tooltip {
+      position: relative;
+      &:after {
+        position: absolute;
+        left: 150%;
+        min-width: 100px;
+        content: attr(data-tooltip);
+        opacity: 0;
+        transition: all .3s ease-in-out .35s;
+        visibility: hidden;
+        z-index: 2;
+        position: absolute;
+        background-color: #eee;
+        padding: 0.75em;
+        border-radius: 3px;
+        font-size: 0.8em;
+      }
+      &:hover:after {
+        opacity: 1;
+        left: 130%;
+        visibility: visible;
+      }
+      .fresh-tooltip-icon {
+        position: relative;
+        height: 0.8em;
+        width: 0.8em;
+        min-height: 0.8em;
+        min-width: 0.8em;
+      }
     }
   }
-}
 
+  .fresh-input-tags {
+    margin-top: 1rem;
+  }
 
-.fresh-switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-  margin: 0.5em 0px;
-}
-
-.fresh-switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.fresh-slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  transition: 0.4s;
-  border-radius: 34px;
-}
-
-.fresh-slider:before {
-  position: absolute;
-  content: '';
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  transition: 0.4s;
-  border-radius: 50%;
-}
-
-input:checked + .fresh-slider {
-  background-color: #2196f3;
-}
-
-input:focus + .fresh-slider {
-  box-shadow: 0 0 1px #2196f3;
-}
-
-input:checked + .fresh-slider:before {
-  transform: translateX(26px);
-}
-
-input,
-select,
-textarea,
-.fresh-input {
-  background-color: transparent;
-  box-sizing: border-box;
-  box-shadow: none;
-  max-width: 100%;
-  border-width: 1px;
-  border-style: solid;
-  border-image: initial;
-  border-color: inherit;
-  border-radius: 3px;
-  font: inherit;
-  margin: 0.5em 0px;
-  padding: 0.75em;
-  transition: border-color 0.2s ease 0s;
-}
-
-textarea,
-.fresh-input-textarea,
-.fresh-input-markdown {
-  min-height: 6rem;
-}
-
-.fresh-input-markdown {
-  border-color: #eee; 
-}
-
-.fresh-title {
-  display: block;
-}
-
-.react-tagsinput-tag {
-  background: #eee;
-  padding: 6px 10px;
-  margin: 0 6px 0 0;
-}
-.react-tagsinput-input {
-  margin-top: 1rem;
-  display: block;
-}
-
+  .react-tagsinput-tag {
+    background: #eee;
+    padding: 6px 10px;
+    margin: 0 6px 0 0;
+  }
+  .react-tagsinput-input {
+    margin-top: 1rem;
+    display: block;
+  }
 `
 
 export default Global
